a693323cb028da86528ce1754eb9889b0ba7bd53
Mahmoud Ahmed <UGS.141490@ci.suez.edu.eg>
2024-09-02 11:15:35 +0300

    feat: Update Movie entity class operator methods

--- lib/movies/domain/entities/movie.dart
+++ lib/movies/domain/entities/movie.dart
@@ -14,4 +14,24 @@ class Movie {
     required this.overview,
     required this.voteAverage,
   });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Movie &&
+          other.id == id &&
+          other.title == title &&
+          other.backdropPath == backdropPath &&
+          other.genreIds == genreIds &&
+          other.overview == overview &&
+          other.voteAverage == voteAverage;
+
+  @override
+  int get hashCode =>
+      id.hashCode ^
+      title.hashCode ^
+      backdropPath.hashCode ^
+      genreIds.hashCode ^
+      overview.hashCode ^
+      voteAverage.hashCode;
 }
