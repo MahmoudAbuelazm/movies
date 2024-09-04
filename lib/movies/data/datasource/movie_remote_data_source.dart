98bb3143043e5e1ee20b05f2abbdf70f8fb953c1
Mahmoud Ahmed <UGS.141490@ci.suez.edu.eg>
2024-09-04 11:56:10 +0300

    refactor: Update MovieRemoteDataSource to fetch now playing movies

--- lib/movies/data/datasource/movie_remote_data_source.dart
+++ lib/movies/data/datasource/movie_remote_data_source.dart
@@ -1,3 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:movies/movies/data/models/movie_model.dart';
+
+import '../../../core/utils/app_constants.dart';
+
 class MovieRemoteDataSource {
-  getNowPlayingMovies() {}
+  Future<List<MovieModel>> getNowPlayingMovies() async {
+    final respone = await Dio().get(AppConstants.baseUrl);
+
+    if (respone.statusCode == 200) {
+      return respone.data;
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
 }
