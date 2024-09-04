88fa34a05cac1eaf836cd550ff7951030acbab07
Mahmoud Ahmed <UGS.141490@ci.suez.edu.eg>
2024-09-04 11:45:07 +0300

    refactor: Update MovieModel constructor to use named parameters

--- lib/movies/data/models/movie_model.dart
+++ lib/movies/data/models/movie_model.dart
@@ -1,12 +1,13 @@
 import 'package:movies/movies/domain/entities/movie.dart';
 
 class MovieModel extends Movie {
-  const MovieModel(
-      {required super.id,
-      required super.title,
-      required super.backdropPath,
-      required super.genreIds,
-      required super.overview,
-      required super.voteAverage,
-      required super.releaseDate});
+  const MovieModel({
+    required super.id,
+    required super.title,
+    required super.backdropPath,
+    required super.genreIds,
+    required super.overview,
+    required super.voteAverage,
+    required super.releaseDate,
+  });
 }
