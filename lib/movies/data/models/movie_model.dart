6ec7748c4b40cfbec2e257c8f36702f64611d15e
Mahmoud Ahmed <UGS.141490@ci.suez.edu.eg>
2024-09-04 11:44:16 +0300

    refactor: Rename MoviesModel to MovieModel

--- lib/movies/data/models/movie_model.dart
+++ lib/movies/data/models/movie_model.dart
@@ -1,7 +1,7 @@
 import 'package:movies/movies/domain/entities/movie.dart';
 
-class MoviesModel extends Movie {
-  const MoviesModel(
+class MovieModel extends Movie {
+  const MovieModel(
       {required super.id,
       required super.title,
       required super.backdropPath,
