f1c412cf350070c0b5373e8f0a19b3cdd9f593da
Mahmoud Ahmed <UGS.141490@ci.suez.edu.eg>
2024-09-05 11:29:13 +0300

    refactor: Update API constants in ApiConstants class

--- lib/core/network/api_constants.dart
+++ lib/core/network/api_constants.dart
@@ -1,5 +1,7 @@
 class ApiConstants{
 
-  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String baseUrl = 'https://api.themoviedb.org/3/';
   static const String apiKey = 'dd88e873f4f14dd67463c2e6fd267ae8';
+
+  static const String nowPlaying = '$baseUrl/movie/now_playing?api_key=$apiKey';
 }
