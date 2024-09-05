3f82011c9368094314ca4eb4bbaa3a7c0a188dfd
Mahmoud Ahmed <UGS.141490@ci.suez.edu.eg>
2024-09-05 20:47:31 +0300

    refactor: Update releaseDate type to String in Movie entity

--- lib/movies/presentation/screens/movies_screen.dart
+++ lib/movies/presentation/screens/movies_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:movies/movies/data/datasource/movie_remote_data_source.dart';
+import 'package:movies/movies/data/repository/movies_repository.dart';
+import 'package:movies/movies/domain/repository/base_movie_repository.dart';
+import 'package:movies/movies/domain/usecases/get_now_playing_usecase.dart';
 
 class MoviesScreen extends StatefulWidget {
   const MoviesScreen({super.key});
@@ -9,6 +13,12 @@ class MoviesScreen extends StatefulWidget {
 
 class _MoviesScreenState extends State<MoviesScreen> {
   @override
+  void initState() {
+    _getDate();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -18,5 +28,16 @@ class _MoviesScreenState extends State<MoviesScreen> {
         child: Text('Movies Screen'),
       ),
     );
+  }
+
+  void _getDate() async {
+    BaseMovieRemoteDataSource baseMovieRemoteDataSource =
+        MovieRemoteDataSource();
+
+    BaseMovieRepository baseMovieRepository =
+        MoviesRepository(baseMovieRemoteDataSource);
+
+    final result = await GetNowPlayingUsecase(baseMovieRepository).excute();
+    print(result);
   }
 }
