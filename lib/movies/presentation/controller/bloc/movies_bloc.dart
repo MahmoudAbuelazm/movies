6a8d36c6b1fdc48420320d1255ddd432a7158e25
Mahmoud Ahmed <UGS.141490@ci.suez.edu.eg>
2024-09-05 21:03:06 +0300

    refactor: Update MoviesBloc to include RequestState for managing movie-related events and states

--- lib/movies/presentation/controller/bloc/movies_bloc.dart
+++ lib/movies/presentation/controller/bloc/movies_bloc.dart
@@ -1,6 +1,9 @@
 
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies/core/utils/enums.dart';
+
+import '../../../domain/entities/movie.dart';
 
 part 'movies_event.dart';
 part 'movies_state.dart';
--- lib/movies/presentation/controller/bloc/movies_state.dart
+++ lib/movies/presentation/controller/bloc/movies_state.dart
@@ -1,10 +1,18 @@
 part of 'movies_bloc.dart';
 
-sealed class MoviesState extends Equatable {
-  const MoviesState();
-  
+abstract class MoviesState extends Equatable {
+  final List<Movie> nowPlayingMovies;
+  final RequestState requestState;
+  final String message;
+
+  const MoviesState({
+    this.nowPlayingMovies = const [],
+    this.requestState = RequestState.loading,
+    this.message = '',
+  });
+
   @override
-  List<Object> get props => [];
+  List<Object> get props => [nowPlayingMovies, requestState, message];
 }
 
 final class MoviesInitial extends MoviesState {}
