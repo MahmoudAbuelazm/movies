2232557e712bdaa594357ca4ab66b5212902488d
Mahmoud Ahmed <UGS.141490@ci.suez.edu.eg>
2024-09-05 14:32:37 +0300

    refactor: Implement getNowPlaying method in MoviesRepository

--- lib/movies/data/repository/movies_repository.dart
+++ lib/movies/data/repository/movies_repository.dart
@@ -1,1 +1,23 @@
-class MoviesRepository{}
+import 'package:movies/movies/domain/entities/movie.dart';
+
+import '../../domain/repository/base_movie_repository.dart';
+
+class MoviesRepository extends BaseMovieRepository{
+  @override
+  Future<List<Movie>> getNowPlaying() {
+    // TODO: implement getNowPlaying
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Movie>> getPopular() {
+    // TODO: implement getPopular
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Movie>> getTopRated() {
+    // TODO: implement getTopRated
+    throw UnimplementedError();
+  }
+}
