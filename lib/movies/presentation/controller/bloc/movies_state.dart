38d9ea32cb5b908c12182c33cbd61d5fa783c7ad
Mahmoud Ahmed <UGS.141490@ci.suez.edu.eg>
2024-09-05 21:08:50 +0300

    refactor: Update MoviesState to include nowPlayingMessage instead of message

--- lib/movies/presentation/controller/bloc/movies_state.dart
+++ lib/movies/presentation/controller/bloc/movies_state.dart
@@ -3,16 +3,16 @@ part of 'movies_bloc.dart';
 abstract class MoviesState extends Equatable {
   final List<Movie> nowPlayingMovies;
   final RequestState requestState;
-  final String message;
+  final String nowPlayingMessage;
 
   const MoviesState({
     this.nowPlayingMovies = const [],
     this.requestState = RequestState.loading,
-    this.message = '',
+    this.nowPlayingMessage = '',
   });
 
   @override
-  List<Object> get props => [nowPlayingMovies, requestState, message];
+  List<Object> get props => [nowPlayingMovies, requestState, nowPlayingMessage];
 }
 
 final class MoviesInitial extends MoviesState {}
