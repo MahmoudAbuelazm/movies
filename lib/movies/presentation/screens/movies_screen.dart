e3ffedfdf7e24e65e8f6ca3115fa4b44571fabb2
Mahmoud Ahmed <UGS.141490@ci.suez.edu.eg>
2024-09-05 20:40:46 +0300

    refactor: Add MoviesScreen to the project

--- lib/movies/presentation/screens/movies_screen.dart
+++ lib/movies/presentation/screens/movies_screen.dart
@@ -1,1 +1,22 @@
-stlf
+import 'package:flutter/material.dart';
+
+class MoviesScreen extends StatefulWidget {
+  const MoviesScreen({super.key});
+
+  @override
+  State<MoviesScreen> createState() => _MoviesScreenState();
+}
+
+class _MoviesScreenState extends State<MoviesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Movies'),
+      ),
+      body: const Center(
+        child: Text('Movies Screen'),
+      ),
+    );
+  }
+}
