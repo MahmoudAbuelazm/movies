12764cce86e41df92c500f85ccc3ae7bb7ba843b
Mahmoud Ahmed <UGS.141490@ci.suez.edu.eg>
2024-09-05 20:54:35 +0300

    refactor: Remove unused code in MoviesScreen

--- lib/movies/presentation/screens/movies_screen.dart
+++ lib/movies/presentation/screens/movies_screen.dart
@@ -1,22 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:movies/movies/data/datasource/movie_remote_data_source.dart';
-import 'package:movies/movies/data/repository/movies_repository.dart';
-import 'package:movies/movies/domain/repository/base_movie_repository.dart';
-import 'package:movies/movies/domain/usecases/get_now_playing_usecase.dart';
 
-class MoviesScreen extends StatefulWidget {
+class MoviesScreen extends StatelessWidget {
   const MoviesScreen({super.key});
-
-  @override
-  State<MoviesScreen> createState() => _MoviesScreenState();
-}
-
-class _MoviesScreenState extends State<MoviesScreen> {
-  @override
-  void initState() {
-    _getDate();
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,14 +15,4 @@ class _MoviesScreenState extends State<MoviesScreen> {
     );
   }
 
-  void _getDate() async {
-    BaseMovieRemoteDataSource baseMovieRemoteDataSource =
-        MovieRemoteDataSource();
-
-    BaseMovieRepository baseMovieRepository =
-        MoviesRepository(baseMovieRemoteDataSource);
-
-    final result = await GetNowPlayingUsecase(baseMovieRepository).excute();
-    
-  }
 }
