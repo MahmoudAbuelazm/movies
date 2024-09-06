afed7250a9b0775e1080c3b8889f147766a834bb
Mahmoud Ahmed <UGS.141490@ci.suez.edu.eg>
2024-09-06 17:24:10 +0300

    refactor: Update MoviesBloc to include lazy loading for images

--- lib/movies/presentation/screens/movies_screen.dart
+++ lib/movies/presentation/screens/movies_screen.dart
@@ -7,6 +7,8 @@ import 'package:shimmer/shimmer.dart';
 
 import '../../../core/network/api_constants.dart';
 import '../../../core/utils/dummy.dart';
+import '../components/now_playing_component.dart';
+import '../components/popular_component.dart';
 
 class MainMoviesScreen extends StatelessWidget {
   const MainMoviesScreen({super.key});
@@ -20,95 +22,7 @@ class MainMoviesScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            FadeIn(
-              duration: const Duration(milliseconds: 500),
-              child: CarouselSlider(
-                options: CarouselOptions(
-                  height: 400.0,
-                  viewportFraction: 1.0,
-                  onPageChanged: (index, reason) {},
-                ),
-                items: moviesList.map(
-                  (item) {
-                    return GestureDetector(
-                      key: const Key('openMovieMinimalDetail'),
-                      onTap: () {
-                        /// TODO : NAVIGATE TO MOVIE DETAILS
-                      },
-                      child: Stack(
-                        children: [
-                          ShaderMask(
-                            shaderCallback: (rect) {
-                              return const LinearGradient(
-                                begin: Alignment.topCenter,
-                                end: Alignment.bottomCenter,
-                                colors: [
-                                  // fromLTRB
-                                  Colors.transparent,
-                                  Colors.black,
-                                  Colors.black,
-                                  Colors.transparent,
-                                ],
-                                stops: [0, 0.3, 0.5, 1],
-                              ).createShader(
-                                Rect.fromLTRB(0, 0, rect.width, rect.height),
-                              );
-                            },
-                            blendMode: BlendMode.dstIn,
-                            child: CachedNetworkImage(
-                              height: 560.0,
-                              imageUrl:
-                                  ApiConstants.imageUrl(item.backdropPath),
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          Align(
-                            alignment: Alignment.bottomCenter,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(bottom: 16.0),
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      const Icon(
-                                        Icons.circle,
-                                        color: Colors.redAccent,
-                                        size: 16.0,
-                                      ),
-                                      const SizedBox(width: 4.0),
-                                      Text(
-                                        'Now Playing'.toUpperCase(),
-                                        style: const TextStyle(
-                                          fontSize: 16.0,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(bottom: 16.0),
-                                  child: Text(
-                                    item.title,
-                                    textAlign: TextAlign.center,
-                                    style: const TextStyle(
-                                      fontSize: 24,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                ).toList(),
-              ),
-            ),
+            const NowPlayingComponent(),
             Container(
               margin: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 8.0),
               child: Row(
@@ -145,52 +59,7 @@ class MainMoviesScreen extends StatelessWidget {
                 ],
               ),
             ),
-            FadeIn(
-              duration: const Duration(milliseconds: 500),
-              child: SizedBox(
-                height: 170.0,
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  itemCount: moviesList.length,
-                  itemBuilder: (context, index) {
-                    final movie = moviesList[index];
-                    return Container(
-                      padding: const EdgeInsets.only(right: 8.0),
-                      child: InkWell(
-                        onTap: () {
-                          /// TODO : NAVIGATE TO  MOVIE DETAILS
-                        },
-                        child: ClipRRect(
-                          borderRadius:
-                              const BorderRadius.all(Radius.circular(8.0)),
-                          child: CachedNetworkImage(
-                            width: 120.0,
-                            fit: BoxFit.cover,
-                            imageUrl: ApiConstants.imageUrl(movie.backdropPath),
-                            placeholder: (context, url) => Shimmer.fromColors(
-                              baseColor: Colors.grey[850]!,
-                              highlightColor: Colors.grey[800]!,
-                              child: Container(
-                                height: 170.0,
-                                width: 120.0,
-                                decoration: BoxDecoration(
-                                  color: Colors.black,
-                                  borderRadius: BorderRadius.circular(8.0),
-                                ),
-                              ),
-                            ),
-                            errorWidget: (context, url, error) =>
-                                const Icon(Icons.error),
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
+            const PopularComponent(),
             Container(
               margin: const EdgeInsets.fromLTRB(
                 16.0,
