beb51545e05a4b1ac9048695b107b07c4292a108
Mahmoud Ahmed <UGS.141490@ci.suez.edu.eg>
2024-09-06 15:51:16 +0300

    refactor: Update MoviesBloc to include GetNowPlayingMovies use case

--- lib/movies/presentation/controller/bloc/movies_bloc.dart
+++ lib/movies/presentation/controller/bloc/movies_bloc.dart
@@ -1,8 +1,11 @@
-
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies/core/utils/enums.dart';
+import 'package:movies/movies/data/datasource/movie_remote_data_source.dart';
+import 'package:movies/movies/domain/repository/base_movie_repository.dart';
+import 'package:movies/movies/domain/usecases/get_now_playing_usecase.dart';
 
+import '../../../data/repository/movies_repository.dart';
 import '../../../domain/entities/movie.dart';
 
 part 'movies_event.dart';
@@ -10,6 +13,11 @@ part 'movies_state.dart';
 
 class MoviesBloc extends Bloc<MoviesEvent, MoviesState> {
   MoviesBloc() : super(const MoviesState()) {
-    on<GetNowPlayingMovies>((event, emit) {});
+    on<GetNowPlayingMovies>((event, emit) async {
+      BaseMovieRemoteDataSource movieRemoteDataSource = MovieRemoteDataSource();
+      BaseMovieRepository movieRepository =
+          MoviesRepository(movieRemoteDataSource);
+      final result = await GetNowPlayingUsecase(movieRepository).excute();
+    });
   }
 }
