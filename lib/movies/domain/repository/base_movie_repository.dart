60c54cb96283e210c4303bcb29add39e18672ad3
Mahmoud Ahmed <UGS.141490@ci.suez.edu.eg>
2024-09-07 13:13:26 +0300

    feat: Add GetMovieDetailsParams class for passing movieId to getMovieDetails method

--- lib/movies/domain/repository/base_movie_repository.dart
+++ lib/movies/domain/repository/base_movie_repository.dart
@@ -3,11 +3,12 @@ import 'package:movies/movies/domain/entities/movie_details.dart';
 
 import '../../../core/error/failure.dart';
 import '../entities/movie.dart';
+import '../usecases/get_movie_details_usecase.dart';
 
 abstract class BaseMovieRepository {
   Future<Either<Failure, List<Movie>>> getNowPlaying();
   Future<Either<Failure, List<Movie>>> getPopular();
   Future<Either<Failure, List<Movie>>> getTopRated();
 
-  Future<Either<Failure, MovieDetails>> getMovieDetails(int movieId);
+  Future<Either<Failure, MovieDetails>> getMovieDetails(GetMovieDetailsParams movieId);
 }
